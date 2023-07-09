9ba2b53b461ecafc76616bb9cecbae270c0a9f6b
dershun <cds*963>
2023-07-09 13:21:32 +0800

    initialize

---
+++ src/Singleton.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * @Author: juneChen && juneswoole@163.com
+ * @Date: 2023-07-09 11:41:56
+ * @LastEditors: juneChen && juneswoole@163.com
+ * @LastEditTime: 2023-07-09 11:55:12
+ * @Description: 单例 trait
+ * 
+ */
+
+namespace JuneSwoole\Component;
+
+trait Singleton
+{
+    private static $instance;
+
+    public static function getInstance(...$args)
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new static(...$args);
+        }
+        return self::$instance;
+    }
+}
